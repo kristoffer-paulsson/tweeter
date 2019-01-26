8e71e49bebc7a08ba937f60d768c978b593c64cc
Kristoffer Paulsson <kristoffer.paulsson@talenten.se>
2019-01-26 18:29:34 +0100

    Made some fixes.

--- main.py
+++ main.py
@@ -1,12 +1,11 @@
 import datetime
 import time
-import logging
 import re
 from random import Random
 
 import yaml
 from twitter import Twitter, OAuth
-from flask import Flask, abort
+from flask import Flask
 
 
 app = Flask(__name__)
@@ -19,21 +18,13 @@ def hello():
 
 @app.route('/retweet/<tlist>/<ttimes>')
 def tweeter(tlist, ttimes='1'):
-    logger.info('/retweet/' + tlist + '/' + ttimes)
     ttimes = int(ttimes) if int(ttimes) > 0 else 1
 
-    try:
-        logger.info('Loading configuration.')
-        with open('./config.yaml', 'r') as stream:
-            config = yaml.load(stream)
+    with open('./config.yaml', 'r') as stream:
+        config = yaml.load(stream)
 
-        logger.info('Loading tweets.')
-        with open('tweets/' + tlist + '.yaml', 'r') as stream:
-            tweets = yaml.load(stream)
-
-    except Exception as e:
-        logger.error('Failed to load confugration or data.', e)
-        abort(500)
+    with open('tweets/' + tlist + '.yaml', 'r') as stream:
+        tweets = yaml.load(stream)
 
     today = datetime.datetime.now()
     Random(today.year + int(ttimes/24*today.hour)).shuffle(tweets)
@@ -41,23 +32,14 @@ def tweeter(tlist, ttimes='1'):
     tweet = regex.findall(
         tweets[len(tweets) & today.timetuple().tm_yday])[0]
 
-    try:
-        logger.info('Authenticates against Twitter OAuth.')
-        twitter = Twitter(auth=OAuth(
-            config['token'], config['token_secret'],
-            config['consumer_key'], config['consumer_secret']))
+    twitter = Twitter(auth=OAuth(
+        config['token'], config['token_secret'],
+        config['consumer_key'], config['consumer_secret']))
 
-        logger.info('Unretweet status with id: ' + tweet)
-        twitter.statuses.unretweet(id=tweet)
-        time.sleep(5)
-        logger.info('Retweet status with id: ' + tweet)
-        twitter.statuses.retweet(id=tweet)
-    except Exception as e:
-        logger.error('Failed to interact with twitter', e)
-        abort(500)
+    twitter.statuses.unretweet(id=tweet)
+    time.sleep(5)
+    twitter.statuses.retweet(id=tweet)
 
-
-logger = logging.getLogger()
 
 if __name__ == '__main__':
-    app.run(host='127.0.0.1', port=8080, debug=True)
+    app.run()
