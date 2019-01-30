dd33f934087aa4ba5a59b10ef7548fd22baed906
Kristoffer Paulsson <kristoffer.paulsson@talenten.se>
2019-01-30 14:36:55 +0100

    Added support for shoutout.

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import datetime
 import time
 import re
+import json
 from random import Random
 
 import yaml
@@ -37,6 +38,37 @@ def tweeter(tlist, ttimes='1'):
     twitter.statuses.unretweet(id=tweet)
     time.sleep(5)
     twitter.statuses.retweet(id=tweet)
+
+    return '', 200
+
+
+@app.route('/shoutout')
+def shoutout():
+    if 'X-Appengine-Cron' not in request.headers:
+        return abort(400)
+
+    with open('./config.yaml', 'r') as stream:
+        config = yaml.load(stream)
+
+    twitter = Twitter(auth=OAuth(
+        config['token'], config['token_secret'],
+        config['consumer_key'], config['consumer_secret']))
+
+    statuses = json.loads(twitter.statuses.retweets_of_me(
+        count=100, include_user_entities=True))
+
+    today = datetime.date.today()
+    screen_names = []
+    for i in statuses:
+        created = datetime.datetime.strptime(
+            i['created_at'], '%a %b %d %H:%M:%S +0000 %Y').date()
+        if today == created:  # FIX last 24 hours
+            screen_names.append('@' + i['user']['screen_name'])
+
+    if len(screen_names):
+        time.sleep(3)
+        twitter.statuses.update(
+            status='Thank you for RT!\n\n' + ', '.join(screen_names))
 
     return '', 200
 
