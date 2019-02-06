6b63ffce0959ef946e2c036b25e733d82d166916
Kristoffer Paulsson <kristoffer.paulsson@talenten.se>
2019-02-06 16:47:01 +0100

    Totally ranmdom

--- main.py
+++ main.py
@@ -11,7 +11,32 @@ from flask import Flask, request, abort
 app = Flask(__name__)
 
 
-@app.route('/retweet/<tlist>/<ttimes>')
+@app.route('/retweet/<tlist>')
+def random(tlist):
+    if 'X-Appengine-Cron' not in request.headers:
+        return abort(400)
+
+    with open('./config.yaml', 'r') as stream:
+        config = yaml.load(stream)
+
+    with open('tweets/' + tlist + '.yaml', 'r') as stream:
+        tweets = yaml.load(stream)
+
+    Random().shuffle(tweets)
+    regex = re.compile('\/([\d].*)$')
+    tweet = regex.findall(tweets[0])[0]
+
+    twitter = Twitter(auth=OAuth(
+        config['token'], config['token_secret'],
+        config['consumer_key'], config['consumer_secret']))
+
+    twitter.statuses.unretweet(id=tweet)
+    time.sleep(5)
+    twitter.statuses.retweet(id=tweet)
+
+    return '', 200
+
+
 def tweeter(tlist, ttimes='1'):
     if 'X-Appengine-Cron' not in request.headers:
         return abort(400)
@@ -39,36 +64,6 @@ def tweeter(tlist, ttimes='1'):
     twitter.statuses.retweet(id=tweet)
 
     return '', 200
-
-
-@app.route('/shoutout')
-def shoutout():
-    # if 'X-Appengine-Cron' not in request.headers:
-    #    return abort(400)
-
-    with open('./config.yaml', 'r') as stream:
-        config = yaml.load(stream)
-
-    twitter = Twitter(auth=OAuth(
-        config['token'], config['token_secret'],
-        config['consumer_key'], config['consumer_secret']))
-
-    statuses = twitter.statuses.mentions_timeline(count=100)
-
-    today = datetime.date.today()
-    screen_names = []
-    for i in statuses:
-        created = datetime.datetime.strptime(
-            i['created_at'], '%a %b %d %H:%M:%S +0000 %Y').date()
-        if today == created and i['retweeted']:  # FIX last 24 hours
-            screen_names.append('@' + i['user']['screen_name'])
-
-    if len(screen_names):
-        time.sleep(3)
-        twitter.statuses.update(
-            status='Thank you for RT!\n\n' + ', '.join(screen_names))
-
-    return str(len(screen_names)), 200
 
 
 if __name__ == '__main__':
